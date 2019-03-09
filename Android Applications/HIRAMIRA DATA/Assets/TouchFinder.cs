11fe45a9fd8d918e437d13f77ac29ba3b729a363
SamuraiWarmonger <misc.samchris@gmail.com>
2019-03-09 12:19:23 +0530

    Update Appliction

--- Android Applications/HIRAMIRA DATA/Assets/TouchFinder.cs
+++ Android Applications/HIRAMIRA DATA/Assets/TouchFinder.cs
@@ -1,16 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TouchFinder : MonoBehaviour {
-
-	// Use this for initialization
+    public Text t;
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        t.text= PlayerPrefs.GetString("BED", "001"); 
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("DataEntry");
+        }
+    }
 }
