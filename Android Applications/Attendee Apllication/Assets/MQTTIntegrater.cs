4cd38cbbba2f01481f400b104ede6f7f58c0398b
SamuraiWarmonger <misc.samchris@gmail.com>
2019-03-09 16:17:01 +0530

    MQTT CLIENT IMPORT

---
+++ Android Applications/Attendee Apllication/Assets/MQTTIntegrater.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+using uPLibrary.Networking.M2Mqtt.Utility;
+using System.Net;
+
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+public class MQTTIntegrater : MonoBehaviour {
+
+    public TMPro.TMP_Text t;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
